af7f381dada539757b839531b9af20a4ae122120
PragyaGitProfile <86348548+PragyaChaudhary54@users.noreply.github.com>
2026-09-06 03:14:45 +0000

    constants and veriables

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const name="Pragya Chaudhary"
+var company="Cisco"
+let empId=980
+
+console.log(name)
+console.log(company)
+console.log(empId)
+company="SmartWorking";
+console.table([name,company,empId])
