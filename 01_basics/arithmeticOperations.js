3ecf582c602679dbeb45affee09ebdffc3ac5bd4
PragyaGitProfile <86348548+PragyaChaudhary54@users.noreply.github.com>
2026-09-09 12:37:34 +0000

    String Operations

--- 01_basics/arithmeticOperations.js
+++ 01_basics/arithmeticOperations.js
@@ -4,4 +4,6 @@ let multi=3*8
 let div=15/3 // will give division
 let mode=17%5 //will give reminder
 let power=2**5
+let negativeValue=-sum
 console.table([sum,sub,multi,div,mode,power])
+console.log(negativeValue)
---
+++ 01_basics/stringOperations.js
@@ -0,0 +1,12 @@
+let s1="Pragya"
+let s2=" Chaudhary"
+console.log(s1+s2)//strings will add
+console.log(1+2)// add the numbers
+console.log(1+"2")// 12 as 2 is takem as string
+console.log("1"+2)//12 as 1 taken as string
+console.log("1"+"2")//12 as both 1 & 2 are taken as string
+console.log(1+2+"3")//sum 1 and 2 = 3 and 3rd number taken as string so 1+2=3+string 3 so 33
+console.log("1"+"2"+3+4)//1 & 2 are taken as string and also 3 & 4 both are also as string as the staring operation is string so whole will be treated as string
+/*-----Rule------
+1.All will be treat as the first opeation means if starting is number then add them as number but string will be append if given
+2. If operation start as string then whole will be applied as string not numbers*/
