0972b5aa754b5cb27a0e64865130a35f2854ea49
PragyaGitProfile <86348548+PragyaChaudhary54@users.noreply.github.com>
2026-09-10 05:38:37 +0000

    incrementalOperator(pre and post)

---
+++ 01_basics/incrementOperator.js
@@ -0,0 +1,6 @@
+let x=3;
+const y=x++;
+console.log(`x:${x}, y:${y}`) //sign over tilled window ` for printing x:x value and $ will print its value
+let a=5;
+const b=++a;
+console.log(`a:${a},b:${b}`)
